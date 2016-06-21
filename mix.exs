dac2f2936b7969f3acddba51c98786a5ea1de9d5
Lasse Dalegaard <dalegaard@gmail.com>
2016-06-21 22:22:36 +0200

    Switch to elixir 1.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule CWMP.Protocol.Mixfile do
   def project do
     [app: :cwmp_ex,
      version: "0.1.3",
-     elixir: "~> 1.2",
+     elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
