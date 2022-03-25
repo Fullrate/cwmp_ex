e1708c51b303ea31377ceb409a8b25c51eb42881
Jesper Dalberg <jdnln@yousee.dk>
2022-03-25 09:24:08 +0100

    Fixed config deprecation

--- mix.exs
+++ mix.exs
@@ -2,14 +2,16 @@ defmodule CWMP.Protocol.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :cwmp_ex,
-     version: "0.2.5",
-     elixir: "~> 1.6",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     description: "A module that implements the CWMP protocol",
-     package: package(),
-     deps: deps()]
+    [
+      app: :cwmp_ex,
+      version: "0.2.5",
+      elixir: "~> 1.6",
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      description: "A module that implements the CWMP protocol",
+      package: package(),
+      deps: deps()
+    ]
   end
 
   def application do
@@ -25,11 +27,12 @@ defmodule CWMP.Protocol.Mixfile do
   end
 
   defp deps do
-    [{:mix_test_watch, "~> 0.9.0", only: :dev},
-     {:poison, "~> 4.0.1"},
-     {:xml_builder, "~> 2.1.0"},
-     {:erlsom, "~> 1.4.2"},
-     {:ex_doc, "~> 0.19.1", only: :dev}
-   ]
+    [
+      {:mix_test_watch, "~> 0.9.0", only: :dev},
+      {:poison, "~> 4.0.1"},
+      {:xml_builder, "~> 2.1.0"},
+      {:erlsom, "~> 1.4.2"},
+      {:ex_doc, "~> 0.19.1", only: :dev}
+    ]
   end
 end
