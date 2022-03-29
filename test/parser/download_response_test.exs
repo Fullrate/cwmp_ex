77512beeee433863234bad7453a544ec107813d8
Jesper Dalberg <jdnln@yousee.dk>
2022-03-29 11:31:41 +0200

    Testcases now include the Unknown dateTime from the spec

--- test/parser/download_response_test.exs
+++ test/parser/download_response_test.exs
@@ -3,114 +3,154 @@ defmodule CWMP.Protocol.Parser.DownloadResponseTest do
   import TestHelpers
 
   @sample """
-<SOAP-ENV:Envelope
-    xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/"
-    xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/"
-    xmlns:xsd="http://www.w3.org/2001/XMLSchema"
-    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-    xmlns:cwmp="urn:dslforum-org:cwmp-1-0">
-    <SOAP-ENV:Header>
-      <cwmp:ID SOAP-ENV:mustUnderstand="1">API_aa0642e34b23820801e7642ad7cb536c</cwmp:ID>
-    </SOAP-ENV:Header>
-    <SOAP-ENV:Body>
-      <cwmp:DownloadResponse>
-        <Status>1</Status>
-        <StartTime>2015-01-19T23:08:24</StartTime>
-        <CompleteTime>2015-01-19T23:09:24</CompleteTime>
-      </cwmp:DownloadResponse>
-    </SOAP-ENV:Body>
-</SOAP-ENV:Envelope>
-"""
+  <SOAP-ENV:Envelope
+      xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/"
+      xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/"
+      xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xmlns:cwmp="urn:dslforum-org:cwmp-1-0">
+      <SOAP-ENV:Header>
+        <cwmp:ID SOAP-ENV:mustUnderstand="1">API_aa0642e34b23820801e7642ad7cb536c</cwmp:ID>
+      </SOAP-ENV:Header>
+      <SOAP-ENV:Body>
+        <cwmp:DownloadResponse>
+          <Status>1</Status>
+          <StartTime>2015-01-19T23:08:24</StartTime>
+          <CompleteTime>2015-01-19T23:09:24</CompleteTime>
+        </cwmp:DownloadResponse>
+      </SOAP-ENV:Body>
+  </SOAP-ENV:Envelope>
+  """
 
-  @sample_result {:ok,%{cwmp_version: "1-0", entries: [%CWMP.Protocol.Messages.DownloadResponse{
-        status: 1,
-        complete_time: generate_datetime({{19,1,2015},{23,9,24}}),
-        start_time: generate_datetime({{19,1,2015},{23,8,24}})
-      }],
-    header: %CWMP.Protocol.Messages.Header{hold_requests: false, id: "API_aa0642e34b23820801e7642ad7cb536c",
-      session_timeout: 30, no_more_requests: false}}}
+  @sample_result {:ok,
+                  %{
+                    cwmp_version: "1-0",
+                    entries: [
+                      %CWMP.Protocol.Messages.DownloadResponse{
+                        status: 1,
+                        complete_time: generate_datetime({{19, 1, 2015}, {23, 9, 24}}),
+                        start_time: generate_datetime({{19, 1, 2015}, {23, 8, 24}})
+                      }
+                    ],
+                    header: %CWMP.Protocol.Messages.Header{
+                      hold_requests: false,
+                      id: "API_aa0642e34b23820801e7642ad7cb536c",
+                      session_timeout: 30,
+                      no_more_requests: false
+                    }
+                  }}
 
   test "parses DownloadResponse request" do
     assert(CWMP.Protocol.Parser.parse(@sample) == @sample_result)
   end
 
   @sample2 """
-<SOAP-ENV:Envelope
-    xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/"
-    xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/"
-    xmlns:xsd="http://www.w3.org/2001/XMLSchema"
-    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-    xmlns:cwmp="urn:dslforum-org:cwmp-1-0">
-    <SOAP-ENV:Header>
-      <cwmp:ID SOAP-ENV:mustUnderstand="1">API_aa0642e34b23820801e7642ad7cb536c</cwmp:ID>
-    </SOAP-ENV:Header>
-    <SOAP-ENV:Body>
-      <cwmp:DownloadResponse>
-        <Status>2</Status>
-        <StartTime>2015-01-19T23:08:24</StartTime>
-        <CompleteTime>2015-01-19T23:09:24</CompleteTime>
-      </cwmp:DownloadResponse>
-    </SOAP-ENV:Body>
-</SOAP-ENV:Envelope>
-"""
+  <SOAP-ENV:Envelope
+      xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/"
+      xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/"
+      xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xmlns:cwmp="urn:dslforum-org:cwmp-1-0">
+      <SOAP-ENV:Header>
+        <cwmp:ID SOAP-ENV:mustUnderstand="1">API_aa0642e34b23820801e7642ad7cb536c</cwmp:ID>
+      </SOAP-ENV:Header>
+      <SOAP-ENV:Body>
+        <cwmp:DownloadResponse>
+          <Status>2</Status>
+          <StartTime>2015-01-19T23:08:24</StartTime>
+          <CompleteTime>2015-01-19T23:09:24</CompleteTime>
+        </cwmp:DownloadResponse>
+      </SOAP-ENV:Body>
+  </SOAP-ENV:Envelope>
+  """
 
   test "raise, invalid status" do
-    assert(catch_error(CWMP.Protocol.Parser.parse!(@sample2))==%RuntimeError{message: "Integer does not validate"})
+    assert(
+      catch_error(CWMP.Protocol.Parser.parse!(@sample2)) == %RuntimeError{
+        message: "Integer does not validate"
+      }
+    )
   end
 
   @sample3 """
-<SOAP-ENV:Envelope
-    xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/"
-    xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/"
-    xmlns:xsd="http://www.w3.org/2001/XMLSchema"
-    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-    xmlns:cwmp="urn:dslforum-org:cwmp-1-0">
-    <SOAP-ENV:Header>
-      <cwmp:ID SOAP-ENV:mustUnderstand="1">API_aa0642e34b23820801e7642ad7cb536c</cwmp:ID>
-    </SOAP-ENV:Header>
-    <SOAP-ENV:Body>
-      <cwmp:DownloadResponse>
-        <Status>1</Status>
-        <CompleteTime>2015-01-19T23:09:24</CompleteTime>
-      </cwmp:DownloadResponse>
-    </SOAP-ENV:Body>
-</SOAP-ENV:Envelope>
-"""
+  <SOAP-ENV:Envelope
+      xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/"
+      xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/"
+      xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xmlns:cwmp="urn:dslforum-org:cwmp-1-0">
+      <SOAP-ENV:Header>
+        <cwmp:ID SOAP-ENV:mustUnderstand="1">API_aa0642e34b23820801e7642ad7cb536c</cwmp:ID>
+      </SOAP-ENV:Header>
+      <SOAP-ENV:Body>
+        <cwmp:DownloadResponse>
+          <Status>1</Status>
+          <CompleteTime>2015-01-19T23:09:24</CompleteTime>
+        </cwmp:DownloadResponse>
+      </SOAP-ENV:Body>
+  </SOAP-ENV:Envelope>
+  """
 
-  @sample_result3 {:ok,%{cwmp_version: "1-0", entries: [%CWMP.Protocol.Messages.DownloadResponse{
-        status: 1,
-        complete_time: generate_datetime({{19,1,2015},{23,9,24}}),
-        start_time: nil
-      }],
-    header: %CWMP.Protocol.Messages.Header{hold_requests: false, id: "API_aa0642e34b23820801e7642ad7cb536c",
-      session_timeout: 30, no_more_requests: false}}}
+  @sample_result3 {:ok,
+                   %{
+                     cwmp_version: "1-0",
+                     entries: [
+                       %CWMP.Protocol.Messages.DownloadResponse{
+                         status: 1,
+                         complete_time: generate_datetime({{19, 1, 2015}, {23, 9, 24}}),
+                         start_time: nil
+                       }
+                     ],
+                     header: %CWMP.Protocol.Messages.Header{
+                       hold_requests: false,
+                       id: "API_aa0642e34b23820801e7642ad7cb536c",
+                       session_timeout: 30,
+                       no_more_requests: false
+                     }
+                   }}
 
   test "parses DownloadResponse request, missing StartTime" do
     assert(CWMP.Protocol.Parser.parse(@sample3) == @sample_result3)
   end
 
   @sample4 """
-<SOAP-ENV:Envelope
-    xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/"
-    xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/"
-    xmlns:xsd="http://www.w3.org/2001/XMLSchema"
-    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-    xmlns:cwmp="urn:dslforum-org:cwmp-1-0">
-    <SOAP-ENV:Header>
-      <cwmp:ID SOAP-ENV:mustUnderstand="1">API_aa0642e34b23820801e7642ad7cb536c</cwmp:ID>
-    </SOAP-ENV:Header>
-    <SOAP-ENV:Body>
-      <cwmp:DownloadResponse>
-        <Status>1</Status>
-        <StartTime>foo</StartTime>
-        <CompleteTime>2015-01-19T23:09:24</CompleteTime>
-      </cwmp:DownloadResponse>
-    </SOAP-ENV:Body>
-</SOAP-ENV:Envelope>
-"""
+  <SOAP-ENV:Envelope
+      xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/"
+      xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/"
+      xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xmlns:cwmp="urn:dslforum-org:cwmp-1-0">
+      <SOAP-ENV:Header>
+        <cwmp:ID SOAP-ENV:mustUnderstand="1">API_aa0642e34b23820801e7642ad7cb536c</cwmp:ID>
+      </SOAP-ENV:Header>
+      <SOAP-ENV:Body>
+        <cwmp:DownloadResponse>
+          <Status>1</Status>
+          <StartTime>foo</StartTime>
+          <CompleteTime>2015-01-19T23:09:24</CompleteTime>
+        </cwmp:DownloadResponse>
+      </SOAP-ENV:Body>
+  </SOAP-ENV:Envelope>
+  """
 
+  @sample_result4 {:ok,
+                   %{
+                     cwmp_version: "1-0",
+                     entries: [
+                       %CWMP.Protocol.Messages.DownloadResponse{
+                         status: 1,
+                         complete_time: generate_datetime({{19, 1, 2015}, {23, 9, 24}}),
+                         start_time: generate_datetime({{1, 1, 0001}, {0, 0, 0}})
+                       }
+                     ],
+                     header: %CWMP.Protocol.Messages.Header{
+                       hold_requests: false,
+                       id: "API_aa0642e34b23820801e7642ad7cb536c",
+                       session_timeout: 30,
+                       no_more_requests: false
+                     }
+                   }}
   test "parses DownloadResponse request, invalid StartTime" do
-    assert(catch_error(CWMP.Protocol.Parser.parse!(@sample4))==%RuntimeError{message: "timestring 'foo' is unparseable: :invalid_format"})
+    assert(CWMP.Protocol.Parser.parse(@sample4) == @sample_result4)
   end
-
 end
--- test/parser/upload_response_test.exs
+++ test/parser/upload_response_test.exs
@@ -3,114 +3,155 @@ defmodule CWMP.Protocol.Parser.UploadResponseTest do
   import TestHelpers
 
   @sample """
-<SOAP-ENV:Envelope
-    xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/"
-    xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/"
-    xmlns:xsd="http://www.w3.org/2001/XMLSchema"
-    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-    xmlns:cwmp="urn:dslforum-org:cwmp-1-0">
-    <SOAP-ENV:Header>
-      <cwmp:ID SOAP-ENV:mustUnderstand="1">API_aa0642e34b23820801e7642ad7cb536c</cwmp:ID>
-    </SOAP-ENV:Header>
-    <SOAP-ENV:Body>
-      <cwmp:UploadResponse>
-        <Status>1</Status>
-        <StartTime>2015-01-19T23:08:24</StartTime>
-        <CompleteTime>2015-01-19T23:09:24</CompleteTime>
-      </cwmp:UploadResponse>
-    </SOAP-ENV:Body>
-</SOAP-ENV:Envelope>
-"""
+  <SOAP-ENV:Envelope
+      xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/"
+      xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/"
+      xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xmlns:cwmp="urn:dslforum-org:cwmp-1-0">
+      <SOAP-ENV:Header>
+        <cwmp:ID SOAP-ENV:mustUnderstand="1">API_aa0642e34b23820801e7642ad7cb536c</cwmp:ID>
+      </SOAP-ENV:Header>
+      <SOAP-ENV:Body>
+        <cwmp:UploadResponse>
+          <Status>1</Status>
+          <StartTime>2015-01-19T23:08:24</StartTime>
+          <CompleteTime>2015-01-19T23:09:24</CompleteTime>
+        </cwmp:UploadResponse>
+      </SOAP-ENV:Body>
+  </SOAP-ENV:Envelope>
+  """
 
-  @sample_result {:ok,%{cwmp_version: "1-0", entries: [%CWMP.Protocol.Messages.UploadResponse{
-        status: 1,
-        complete_time: generate_datetime({{19,1,2015},{23,9,24}}),
-        start_time: generate_datetime({{19,1,2015},{23,8,24}})
-      }],
-    header: %CWMP.Protocol.Messages.Header{hold_requests: false, id: "API_aa0642e34b23820801e7642ad7cb536c",
-      session_timeout: 30, no_more_requests: false}}}
+  @sample_result {:ok,
+                  %{
+                    cwmp_version: "1-0",
+                    entries: [
+                      %CWMP.Protocol.Messages.UploadResponse{
+                        status: 1,
+                        complete_time: generate_datetime({{19, 1, 2015}, {23, 9, 24}}),
+                        start_time: generate_datetime({{19, 1, 2015}, {23, 8, 24}})
+                      }
+                    ],
+                    header: %CWMP.Protocol.Messages.Header{
+                      hold_requests: false,
+                      id: "API_aa0642e34b23820801e7642ad7cb536c",
+                      session_timeout: 30,
+                      no_more_requests: false
+                    }
+                  }}
 
   test "parses UploadResponse" do
     assert(CWMP.Protocol.Parser.parse(@sample) == @sample_result)
   end
 
   @sample2 """
-<SOAP-ENV:Envelope
-    xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/"
-    xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/"
-    xmlns:xsd="http://www.w3.org/2001/XMLSchema"
-    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-    xmlns:cwmp="urn:dslforum-org:cwmp-1-0">
-    <SOAP-ENV:Header>
-      <cwmp:ID SOAP-ENV:mustUnderstand="1">API_aa0642e34b23820801e7642ad7cb536c</cwmp:ID>
-    </SOAP-ENV:Header>
-    <SOAP-ENV:Body>
-      <cwmp:UploadResponse>
-        <Status>2</Status>
-        <StartTime>2015-01-19T23:08:24</StartTime>
-        <CompleteTime>2015-01-19T23:09:24</CompleteTime>
-      </cwmp:UploadResponse>
-    </SOAP-ENV:Body>
-</SOAP-ENV:Envelope>
-"""
+  <SOAP-ENV:Envelope
+      xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/"
+      xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/"
+      xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xmlns:cwmp="urn:dslforum-org:cwmp-1-0">
+      <SOAP-ENV:Header>
+        <cwmp:ID SOAP-ENV:mustUnderstand="1">API_aa0642e34b23820801e7642ad7cb536c</cwmp:ID>
+      </SOAP-ENV:Header>
+      <SOAP-ENV:Body>
+        <cwmp:UploadResponse>
+          <Status>2</Status>
+          <StartTime>2015-01-19T23:08:24</StartTime>
+          <CompleteTime>2015-01-19T23:09:24</CompleteTime>
+        </cwmp:UploadResponse>
+      </SOAP-ENV:Body>
+  </SOAP-ENV:Envelope>
+  """
 
   test "raise, invalid status" do
-    assert(catch_error(CWMP.Protocol.Parser.parse!(@sample2))==%RuntimeError{message: "Integer does not validate"})
+    assert(
+      catch_error(CWMP.Protocol.Parser.parse!(@sample2)) == %RuntimeError{
+        message: "Integer does not validate"
+      }
+    )
   end
 
   @sample3 """
-<SOAP-ENV:Envelope
-    xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/"
-    xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/"
-    xmlns:xsd="http://www.w3.org/2001/XMLSchema"
-    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-    xmlns:cwmp="urn:dslforum-org:cwmp-1-0">
-    <SOAP-ENV:Header>
-      <cwmp:ID SOAP-ENV:mustUnderstand="1">API_aa0642e34b23820801e7642ad7cb536c</cwmp:ID>
-    </SOAP-ENV:Header>
-    <SOAP-ENV:Body>
-      <cwmp:UploadResponse>
-        <Status>1</Status>
-        <CompleteTime>2015-01-19T23:09:24</CompleteTime>
-      </cwmp:UploadResponse>
-    </SOAP-ENV:Body>
-</SOAP-ENV:Envelope>
-"""
+  <SOAP-ENV:Envelope
+      xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/"
+      xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/"
+      xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xmlns:cwmp="urn:dslforum-org:cwmp-1-0">
+      <SOAP-ENV:Header>
+        <cwmp:ID SOAP-ENV:mustUnderstand="1">API_aa0642e34b23820801e7642ad7cb536c</cwmp:ID>
+      </SOAP-ENV:Header>
+      <SOAP-ENV:Body>
+        <cwmp:UploadResponse>
+          <Status>1</Status>
+          <CompleteTime>2015-01-19T23:09:24</CompleteTime>
+        </cwmp:UploadResponse>
+      </SOAP-ENV:Body>
+  </SOAP-ENV:Envelope>
+  """
 
-  @sample_result3 {:ok,%{cwmp_version: "1-0", entries: [%CWMP.Protocol.Messages.UploadResponse{
-        status: 1,
-        complete_time: generate_datetime({{19,1,2015},{23,9,24}}),
-        start_time: nil
-      }],
-    header: %CWMP.Protocol.Messages.Header{hold_requests: false, id: "API_aa0642e34b23820801e7642ad7cb536c",
-      session_timeout: 30, no_more_requests: false}}}
+  @sample_result3 {:ok,
+                   %{
+                     cwmp_version: "1-0",
+                     entries: [
+                       %CWMP.Protocol.Messages.UploadResponse{
+                         status: 1,
+                         complete_time: generate_datetime({{19, 1, 2015}, {23, 9, 24}}),
+                         start_time: nil
+                       }
+                     ],
+                     header: %CWMP.Protocol.Messages.Header{
+                       hold_requests: false,
+                       id: "API_aa0642e34b23820801e7642ad7cb536c",
+                       session_timeout: 30,
+                       no_more_requests: false
+                     }
+                   }}
 
   test "parses UploadResponse request, missing StartTime" do
     assert(CWMP.Protocol.Parser.parse(@sample3) == @sample_result3)
   end
 
   @sample4 """
-<SOAP-ENV:Envelope
-    xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/"
-    xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/"
-    xmlns:xsd="http://www.w3.org/2001/XMLSchema"
-    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-    xmlns:cwmp="urn:dslforum-org:cwmp-1-0">
-    <SOAP-ENV:Header>
-      <cwmp:ID SOAP-ENV:mustUnderstand="1">API_aa0642e34b23820801e7642ad7cb536c</cwmp:ID>
-    </SOAP-ENV:Header>
-    <SOAP-ENV:Body>
-      <cwmp:UploadResponse>
-        <Status>1</Status>
-        <StartTime>foo</StartTime>
-        <CompleteTime>2015-01-19T23:09:24</CompleteTime>
-      </cwmp:UploadResponse>
-    </SOAP-ENV:Body>
-</SOAP-ENV:Envelope>
-"""
+  <SOAP-ENV:Envelope
+      xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/"
+      xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/"
+      xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xmlns:cwmp="urn:dslforum-org:cwmp-1-0">
+      <SOAP-ENV:Header>
+        <cwmp:ID SOAP-ENV:mustUnderstand="1">API_aa0642e34b23820801e7642ad7cb536c</cwmp:ID>
+      </SOAP-ENV:Header>
+      <SOAP-ENV:Body>
+        <cwmp:UploadResponse>
+          <Status>1</Status>
+          <StartTime>foo</StartTime>
+          <CompleteTime>2015-01-19T23:09:24</CompleteTime>
+        </cwmp:UploadResponse>
+      </SOAP-ENV:Body>
+  </SOAP-ENV:Envelope>
+  """
+
+  @sample_result4 {:ok,
+                   %{
+                     cwmp_version: "1-0",
+                     entries: [
+                       %CWMP.Protocol.Messages.UploadResponse{
+                         status: 1,
+                         start_time: generate_datetime({{1, 1, 1}, {0, 0, 0}}),
+                         complete_time: generate_datetime({{19, 1, 2015}, {23, 9, 24}})
+                       }
+                     ],
+                     header: %CWMP.Protocol.Messages.Header{
+                       hold_requests: false,
+                       id: "API_aa0642e34b23820801e7642ad7cb536c",
+                       session_timeout: 30,
+                       no_more_requests: false
+                     }
+                   }}
 
   test "parses UploadResponse request, invalid StartTime" do
-    assert(catch_error(CWMP.Protocol.Parser.parse!(@sample4))==%RuntimeError{message: "timestring 'foo' is unparseable: :invalid_format"})
+    assert(CWMP.Protocol.Parser.parse(@sample4) == @sample_result4)
   end
-
 end
