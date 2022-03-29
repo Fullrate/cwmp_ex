7173c00b38117065c700eb7615f213da6a5130ee
Jesper Dalberg <jdnln@yousee.dk>
2022-03-29 11:29:42 +0200

    version bumped

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule CWMP.Protocol.Mixfile do
   def project do
     [
       app: :cwmp_ex,
-      version: "0.2.6",
+      version: "0.2.7",
       elixir: "~> 1.13",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
